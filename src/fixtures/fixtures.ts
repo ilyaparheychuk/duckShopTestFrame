67fe75830c4d48381a1b6bab03843eb970357068
unknown <ilyaparheychuk@gmail.com>
2026-08-18 23:47:34 +0300

    kostyl for fixture and linter error empty pattern

--- src/fixtures/fixtures.ts
+++ src/fixtures/fixtures.ts
@@ -2,7 +2,8 @@ import { test as base } from '@playwright/test';
 import { UserCredentials } from '../types/types';
 
 export const test = base.extend<{ credentials: UserCredentials }>({
-  credentials: async (_, use) => {
+  // eslint-disable-next-line no-empty-pattern
+  credentials: async ({}, use) => {
     const username = process.env.TEST_USERNAME;
     const email = process.env.TEST_USER_EMAIL;
     const password = process.env.TEST_PASSWORD;
